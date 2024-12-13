f13ef72edb0284064dbcf5ed6a91f520587c2e10
newemail <newemail@example.com>
2024-12-13 16:09:28 +0100

    ztdezr

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { PrismaService } from 'src/prisma.service';
 import * as argon2 from "argon2"
+import * as crypto from'node:crypto'
 @Injectable()
 export class AuthService {
   constructor(private readonly db:PrismaService){}
@@ -11,7 +12,18 @@ export class AuthService {
       })
     
       if(await argon2.verify(user.password,logindata.password)){
-        return "sikeres";
+          const token= crypto.randomBytes(64).toString('hex')
+          await this.db.token.create({
+            data:{
+              token,
+              user:{connect:{id:user.id}}
+            }
+          })
+          return{
+            token,
+            userid:user.id
+          }
+
       } else{
         throw new Error("hibás")
       }
